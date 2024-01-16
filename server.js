ad8f3813262a53951d564ccaebfcac30ae60d5e8
Zach Smith <zachsmith852@gmail.com>
2024-01-16 09:20:57 -0800

    Update PORT const

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs')
 const uuid = require('./Develop/public/helpers/uuid');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
